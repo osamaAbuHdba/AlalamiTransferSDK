93d7f7d0acf603dc8d848f28f7dde9fadba13773
osamaAbuHdba <osamaabuhdba@icloud.com>
2021-10-07 13:09:29 +0300

    First Commite

---
+++ AlalamiTransferSDK/AlalamiTransferSDK/AlalamiTransferSDK.h
@@ -0,0 +1,18 @@
+//
+//  AlalamiTransferSDK.h
+//  AlalamiTransferSDK
+//
+//  Created by Osama Abu Hdba on 07/10/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AlalamiTransferSDK.
+FOUNDATION_EXPORT double AlalamiTransferSDKVersionNumber;
+
+//! Project version string for AlalamiTransferSDK.
+FOUNDATION_EXPORT const unsigned char AlalamiTransferSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlalamiTransferSDK/PublicHeader.h>
+
+
---
+++ AlalamiTransferSDK/AlalamiTransferSDK/Services.swift
@@ -0,0 +1,17 @@
+//
+//  Services.swift
+//  AlalamiTransferSDK
+//
+//  Created by Osama Abu Hdba on 07/10/2021.
+//
+
+import Foundation
+
+public class Services {
+    
+    private init() {}
+    
+    public func welcomeUsers() {
+        print("Welcome to ALalamiTransfair")
+    }
+}
